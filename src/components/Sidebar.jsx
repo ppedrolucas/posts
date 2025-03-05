9bd6b1f41c19db0cb7b18fa7c4aab983f12cf88f
ppedrolucas <pp15lucas@gmail.com>
2025-03-04 22:54:29 -0300

    componente de avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from "phosphor-react";
 
@@ -9,11 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/ppedrolucas.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/ppedrolucas.png"/>
         <strong>Pedro Lucas</strong>
         <span>Developer</span>
       </div>
