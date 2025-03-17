96f132941395261d1c6499e3be83cfb1e9baa0f5
ppedrolucas <pp15lucas@gmail.com>
2025-03-17 12:02:07 -0300

    adicionando mais posts personalizados

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,46 @@ const posts = [
       {
         type: "paragraph",
         content:
-          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+          "Pra descobrir quem é fofoqueiro é só espalhar uma mentira diferente pra cada um. A mentira que chegar até você de volta é do fofoqueiro",
       },
-      { type: "link", content: "jane.design/doctorcare" },
+      { type: "link", content: "kibum.com.br/links" },
     ],
     publishedAt: new Date("2025-03-02 17:00:00"),
+  },
+  {
+    id: 3,
+    author: {
+      avatar_url: "https://github.com/orodrigogo.png",
+      name: "Rodrigo Gonçalves",
+      role: "Software Engineer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala deeeeev 👋" },
+      {
+        type: "paragraph",
+        content:
+          "I'm a Software Engineer. My specialty is developing Android and iOS Mobile Apps with React Native I also have knowledge with Web development",
+      },
+      { type: "link", content: "react-native" },
+    ],
+    publishedAt: new Date("2025-03-17 17:00:00"),
+  },
+  {
+    id: 4,
+    author: {
+      avatar_url: "https://github.com/leandrocostalti.png",
+      name: "Leandro Costa",
+      role: "Professor de informática obcecado por Linux",
+    },
+    content: [
+      { type: "paragraph", content: "Olá turma 👋" },
+      {
+        type: "paragraph",
+        content: "Se você desconfia que tal produto ta caro, não compre.",
+      },
+      { type: "link", content: "https://www.youtube.com/ltitreinamento" },
+    ],
+    publishedAt: new Date("2025-03-17 17:00:00"),
   },
 ];
 
